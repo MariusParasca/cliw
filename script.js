27425be26781ee7596406a5361e6e9af47f6a0cc
MariusParasca <mrs.parasca@gmail.com>
2018-10-26 13:09:16 +0300

    fixed mini bugs; started to work on item page

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 window.onload = function () {
-    if ((location.href.split("/").slice(-1) == "home.html")) {
-        onLoadHome();
+    if ((location.href.split("/").slice(-1) == "accessory_page_list.html")) {
+        onLoadAccessoryPage();
+    } else {
+        onLoadPage();
     }
 };
 
-//------------------------ Home Page ------------------------//
-function onLoadHome() {
-    setHomeEventListeners();
+//------------------------ Tool bar ------------------------//
+function onLoadPage() {
+    setBarEventListeners();
 }
 
-function setHomeEventListeners() {
+function setBarEventListeners() {
     document.getElementsByClassName("dropdownContent")[0].addEventListener("mouseover", maitainMenuHover);
     document.getElementsByClassName("dropdownContent")[0].addEventListener("mouseout", disableMenuHover);
     document.getElementsByClassName("menuLink")[0].addEventListener("mouseover", maitainMenuHover);
@@ -36,4 +38,13 @@ function maitainUserNameHover() {
 function disableUserNameHover() {
     document.getElementById("navUser").style.backgroundImage = "url('./Icons/user_avatar_white_40px.png')"
     document.getElementsByClassName("userName")[0].style.color = "#ffffff"
+}
+
+//------------------------ Accessory Page ------------------------//
+
+function onLoadAccessoryPage() {
+    document.getElementById("navUser").addEventListener("mouseover", maitainUserNameHover);
+    document.getElementById("navUser").addEventListener("mouseout", disableUserNameHover);
+    document.getElementsByClassName("userName")[0].addEventListener("mouseover", maitainUserNameHover);
+    document.getElementsByClassName("userName")[0].addEventListener("mouseout", disableUserNameHover)
 }
